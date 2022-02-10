8895f635eecc36e81742a01e6fd4d7aaaef57bb5
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-02-10 18:55:56 -0500

    Update FE-Switcheroo.sh

--- FE-Switcheroo.sh
+++ FE-Switcheroo.sh
@@ -9,9 +9,9 @@ function fe_menu() {
         choice=$(dialog --backtitle "Front End Switcheroo Currently using $fe" --title " FRONT END MENU " \
             --ok-label OK --cancel-label Exit \
             --menu "What Mode would you like to Switch to?" 25 75 20 \
-            1 "Switch Frontend to EmuStation" \
-            2 "Switch Frontend to Attract Mode" \
-            3 "Switch Frontend to Pegasus FE" \
+            1 "Switch Frontend To EmuStation" \
+            2 "Switch Frontend To Attract Mode" \
+            3 "Switch Frontend To Pegasus FE" \
             2>&1 > /dev/tty)
 
         case "$choice" in
