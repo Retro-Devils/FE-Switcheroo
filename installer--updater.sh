3b86f1601d767ed850aa29f2ac4412fb39e98a28
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-02-10 17:39:10 -0600

    Add files via upload

---
+++ installer--updater.sh
@@ -0,0 +1,16 @@
+if [ -f "$HOME"/RetroPie/retropiemenu/FE-Switcheroo.sh ]; then
+echo "---UPDATING FE SWITCHEROO NOW---"
+sleep 5
+sudo rm -r "$HOME"/RetroPie/retropiemenu/FE-Switcheroo.sh
+wget https://raw.githubusercontent.com/Retro-Devils/FE-Switcheroo/main/FE-Switcheroo.sh -P "$HOME"/RetroPie/retropiemenu/
+sudo wget https://raw.githubusercontent.com/Retro-Devils/FE-Switcheroo/main/switcheroo -P /usr/local/bin
+chmod 755 "$HOME"/RetroPie/retropiemenu/FE-Switcheroo.sh
+sudo chmod 755/usr/local/bin/switcheroo
+else
+echo "---INSTALLING FE SWITCHEROO NOW---"
+sleep 5
+wget https://raw.githubusercontent.com/Retro-Devils/FE-Switcheroo/main/FE-Switcheroo.sh -P "$HOME"/RetroPie/retropiemenu/
+sudo wget https://raw.githubusercontent.com/Retro-Devils/FE-Switcheroo/main/switcheroo -P /usr/local/bin
+chmod 755 "$HOME"/RetroPie/retropiemenu/FE-Switcheroo.sh
+sudo chmod 755/usr/local/bin/switcheroo
+fi
