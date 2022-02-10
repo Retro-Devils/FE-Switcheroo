3f634ad78f09adf0237fa416bf523378b465f810
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-02-10 17:43:04 -0600

    Update installer--updater.sh

--- installer--updater.sh
+++ installer--updater.sh
@@ -12,5 +12,5 @@ sleep 5
 wget https://raw.githubusercontent.com/Retro-Devils/FE-Switcheroo/main/FE-Switcheroo.sh -P "$HOME"/RetroPie/retropiemenu/
 sudo wget https://raw.githubusercontent.com/Retro-Devils/FE-Switcheroo/main/switcheroo -P /usr/local/bin
 chmod 755 "$HOME"/RetroPie/retropiemenu/FE-Switcheroo.sh
-sudo chmod 755/usr/local/bin/switcheroo
+sudo chmod 755 /usr/local/bin/switcheroo
 fi
