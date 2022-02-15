53a5fc081c14a0bc6adf8f1d22538473cba5f2b2
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-02-14 22:05:38 -0500

    Update installer--updater.sh

--- installer--updater.sh
+++ installer--updater.sh
@@ -5,7 +5,7 @@ sudo rm -R "$HOME"/RetroPie/retropiemenu/FE-Switcheroo.sh
 wget https://raw.githubusercontent.com/Retro-Devils/FE-Switcheroo/main/FE-Switcheroo.sh -P "$HOME"/RetroPie/retropiemenu/
 wget https://github.com/Retro-Devils/FE-Switcheroo/blob/main/FE-Switcheroo.png -P "$HOME"/RetroPie/retropiemenu/icons/
 chmod 755 "$HOME"/RetroPie/retropiemenu/FE-Switcheroo.sh
-cp "$HOME"/RetroPie/retropiemenu/FE-Switcheroo.sh -P "$HOME"/.attract/devils-tools/
+cp "$HOME"/RetroPie/retropiemenu/FE-Switcheroo.sh -P "$HOME"/.attract/tools/
 echo "UPDATE COMPLETE"
 sleep 2 
 else
@@ -17,10 +17,9 @@ wget https://github.com/Retro-Devils/FE-Switcheroo/blob/main/FE-Switcheroo.png -
 chmod 755 "$HOME"/RetroPie/retropiemenu/FE-Switcheroo.sh
 sudo chmod 755 /usr/local/bin/switcheroo
 mkdir "$HOME"/.attract/devils-tools/
-cp "$HOME"/RetroPie/retropiemenu/FE-Switcheroo.sh -P "$HOME"/.attract/devils-tools/
-cp "$HOME"/RetroPie/retropiemenu/Devils-Box.sh -P "$HOME"/.attract/devils-tools/
-chmod 755 "$HOME"/.attract/devils-tools/
-chmod 755 "$HOME"/.attract/devils-tools/
+cp "$HOME"/RetroPie/retropiemenu/FE-Switcheroo.sh -P "$HOME"/.attract/tools/
+cp "$HOME"/RetroPie/retropiemenu/Devils-Box.sh -P "$HOME"/.attract/tools/
+chmod 755 "$HOME"/.attract/tools/
 echo "INSTALL COMPLETE"
 sleep 2
 fi
