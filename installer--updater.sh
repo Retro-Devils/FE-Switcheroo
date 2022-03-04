3ea4f8c14b0f790a83ae5a14e5f909412f808bc7
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-03-03 19:41:14 -0600

    Update installer--updater.sh

--- installer--updater.sh
+++ installer--updater.sh
@@ -1,6 +1,7 @@
 if [ -f "$HOME"/RetroPie/retropiemenu/FE-Switcheroo.sh ]; then
 echo "---UPDATING FE SWITCHEROO NOW---"
 sleep 5
+sudo rm -r "$HOME"/RetroPie/retropiemenu/icons/FE-Switcheroo.png
 sudo rm -R "$HOME"/RetroPie/retropiemenu/FE-Switcheroo.sh
 sudo rm -R /usr/local/bin/switcheroo
 wget https://raw.githubusercontent.com/Retro-Devils/FE-Switcheroo/main/FE-Switcheroo.sh -P "$HOME"/RetroPie/retropiemenu/
