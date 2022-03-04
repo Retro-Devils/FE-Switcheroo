a0ee0926aaa2f462978db3d4d3a9759fced1e92d
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-03-03 19:50:33 -0600

    Update installer--updater.sh

--- installer--updater.sh
+++ installer--updater.sh
@@ -4,10 +4,12 @@ sleep 5
 sudo rm -r "$HOME"/RetroPie/retropiemenu/icons/FE-Switcheroo.png
 sudo rm -R "$HOME"/RetroPie/retropiemenu/FE-Switcheroo.sh
 sudo rm -R /usr/local/bin/switcheroo
+sudo rm -r "$HOME"/.attract/tools/FE-Switcheroo.sh
 wget https://raw.githubusercontent.com/Retro-Devils/FE-Switcheroo/main/FE-Switcheroo.sh -P "$HOME"/RetroPie/retropiemenu/
 wget https://github.com/Retro-Devils/FE-Switcheroo/blob/main/FE-Switcheroo.png -P "$HOME"/RetroPie/retropiemenu/icons/
 sudo wget https://raw.githubusercontent.com/Retro-Devils/FE-Switcheroo/main/switcheroo -P /usr/local/bin
 chmod 755 "$HOME"/RetroPie/retropiemenu/FE-Switcheroo.sh
+chmod 755 "$HOME"/.attract/tools/FE-Switcheroo.sh
 sudo chmod 755 /usr/local/bin/switcheroo
 cp "$HOME"/RetroPie/retropiemenu/FE-Switcheroo.sh -P "$HOME"/.attract/tools/
 echo "UPDATE COMPLETE LAUNCHING SWITCHEROO"
