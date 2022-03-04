c7c1ae95a410c3ca9f1ad910815796472dbe5310
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-03-03 19:53:13 -0600

    Update FE-Switcheroo.sh

--- FE-Switcheroo.sh
+++ FE-Switcheroo.sh
@@ -324,7 +324,39 @@ read -n 1 -s -r -p "Above Is Converting Results-----Press any key to Continue"
 }
 
 function update-switcheroo() {
-curl -sSL https://bit.ly/3GH7Oz4 | bash
+if [ -f "$HOME"/RetroPie/retropiemenu/FE-Switcheroo.sh ]; then
+echo "---UPDATING FE SWITCHEROO NOW---"
+sleep 5
+sudo rm -r "$HOME"/RetroPie/retropiemenu/icons/FE-Switcheroo.png
+sudo rm -R "$HOME"/RetroPie/retropiemenu/FE-Switcheroo.sh
+sudo rm -R /usr/local/bin/switcheroo
+sudo rm -r "$HOME"/.attract/tools/FE-Switcheroo.sh
+wget https://raw.githubusercontent.com/Retro-Devils/FE-Switcheroo/main/FE-Switcheroo.sh -P "$HOME"/RetroPie/retropiemenu/
+wget https://github.com/Retro-Devils/FE-Switcheroo/blob/main/FE-Switcheroo.png -P "$HOME"/RetroPie/retropiemenu/icons/
+sudo wget https://raw.githubusercontent.com/Retro-Devils/FE-Switcheroo/main/switcheroo -P /usr/local/bin
+chmod 755 "$HOME"/RetroPie/retropiemenu/FE-Switcheroo.sh
+chmod 755 "$HOME"/.attract/tools/FE-Switcheroo.sh
+sudo chmod 755 /usr/local/bin/switcheroo
+cp "$HOME"/RetroPie/retropiemenu/FE-Switcheroo.sh -P "$HOME"/.attract/tools/
+echo "UPDATE COMPLETE LAUNCHING SWITCHEROO"
+"$HOME"/RetroPie/retropiemenu/FE-Switcheroo.sh
+sleep 2 
+else
+echo "---INSTALLING FE SWITCHEROO NOW---"
+sleep 5
+wget https://raw.githubusercontent.com/Retro-Devils/FE-Switcheroo/main/FE-Switcheroo.sh -P "$HOME"/RetroPie/retropiemenu/
+sudo wget https://raw.githubusercontent.com/Retro-Devils/FE-Switcheroo/main/switcheroo -P /usr/local/bin
+wget https://github.com/Retro-Devils/FE-Switcheroo/blob/main/FE-Switcheroo.png -P "$HOME"/RetroPie/retropiemenu/icons/
+chmod 755 "$HOME"/RetroPie/retropiemenu/FE-Switcheroo.sh
+sudo chmod 755 /usr/local/bin/switcheroo
+mkdir "$HOME"/.attract/tools/
+cp "$HOME"/RetroPie/retropiemenu/FE-Switcheroo.sh -P "$HOME"/.attract/tools/
+cp "$HOME"/RetroPie/retropiemenu/Devils-Box.sh -P "$HOME"/.attract/tools/
+chmod 755 "$HOME"/.attract/tools/
+echo "INSTALL COMPLETE LAUNCHING SWITCHEROO"
+sleep 2
+"$HOME"/RetroPie/retropiemenu/FE-Switcheroo.sh
+fi
 }
 fe_menu
   fi
