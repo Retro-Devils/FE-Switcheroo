c17eb4169a1d8f9ae30af4a4bbed9cf5217262d0
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-07-11 22:18:34 -0400

    Add files via upload

---
+++ buttons/Attract-Mode.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+AUTOSTART="/opt/retropie/configs/all/autostart.sh"
+
+clear
+mode_check
+echo "Front End Switcheroo Currently Using $fe"
+sleep 3 
+if [ ! -d /opt/retropie/configs/all/attractmode ]; then
+dialog  --sleep 1 --title "ATTRACT MODE NOT INSTALLED !! " --msgbox " 
+INSTALLING NOW" 0 0
+cd $HOME/RetroPie-Setup
+sudo ./retropie_packages.sh attractmode
+else
+    if grep -q 'emulationstation \#auto' "$AUTOSTART"; then
+    sudo sed -i 's/emulationstation \#auto/attract \#auto/g' $AUTOSTART
+    echo "Attract Mode Set"
+    sleep 2
+    pgrep -f emulationstation|xargs sudo kill -9 > /dev/null 2>&1 &
+    attract 2>&1
+    elif grep -q 'pegasus-fe \#auto' "$AUTOSTART"; then
+    sudo sed -i 's/pegasus-fe \#auto/attract \#auto/g' $AUTOSTART
+    echo "Attract Mode Set"
+    sleep 2
+    pgrep -f pegasus-fe|xargs sudo kill -9 > /dev/null 2>&1 &
+    attract 2>&1
+    elif grep -q 'startx \#auto' "$AUTOSTART"; then
+    sudo sed -i 's/startx \#auto/attract \#auto/g' $AUTOSTART
+    echo "Attract Mode Set"
+    sleep 2
+    pgrep -f startx|xargs sudo kill -9 > /dev/null 2>&1 &
+    attract 2>&1
+    elif grep -q 'attract \#auto' "$AUTOSTART"; then
+    echo "Attract Mode Already Set"
+    sleep 2
+fi
+fi
+mode_check
+echo "Front End Set Too $fe"
+sleep 3 
+echo "Rebooting Now"
+sleep 3 
+sudo reboot
+
+function mode_check() {
+if grep -q 'emulationstation \#auto' "$AUTOSTART"; then
+	fe="Emulationstation Mode"
+elif grep -q 'attract \#auto' "$AUTOSTART"; then
+	fe="Attract Mode"
+elif grep -q 'pegasus-fe \#auto' "$AUTOSTART"; then
+	fe="Pegasus Mode"
+elif grep -q 'startx \#auto' "$AUTOSTART"; then
+	fe="Desktop Mode"
+fi
+}
---
+++ buttons/Desktop.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+AUTOSTART="/opt/retropie/configs/all/autostart.sh"
+
+clear
+mode_check
+echo "Front End Switcheroo Currently Using $fe"
+sleep 3 
+if [ ! -f /usr/bin/startx ]; then
+dialog  --sleep 1 --title "PIXEL DESKTOP NOT INSTALLED !! " --msgbox " 
+INSTALLING NOW" 0 0
+cd $HOME/RetroPie-Setup
+sudo ./retropie_packages.sh raspbiantools lxde
+else
+    if grep -q 'attract \#auto' "$AUTOSTART"; then
+    sudo sed -i 's/attract \#auto/startx \#auto/g' $AUTOSTART
+    echo "Desktop Mode Set"
+    sleep 2
+    pgrep -f attract|xargs sudo kill -9 > /dev/null 2>&1 &
+    sudo openvt -c 1 -s -f startx 2>&1
+    elif grep -q 'pegasus-fe \#auto' "$AUTOSTART"; then
+    sudo sed -i 's/pegasus-fe \#auto/startx \#auto/g' $AUTOSTART
+    echo "Desktop Mode Set"
+    sleep 2
+    pgrep -f pegasus-fe|xargs sudo kill -9 > /dev/null 2>&1 &
+    sudo openvt -c 1 -s -f startx 2>&1
+    elif grep -q 'emulationstation \#auto' "$AUTOSTART"; then
+    sudo sed -i 's/emulationstation \#auto/startx \#auto/g' $AUTOSTART
+    echo "Desktop Mode Set"
+    sleep 2
+    pgrep -f emulationstation|xargs sudo kill -9 > /dev/null 2>&1 &
+    sudo openvt -c 1 -s -f startx 2>&1
+    elif grep -q 'startx \#auto' "$AUTOSTART"; then
+    echo "Desktop Mode Already Set"
+    sleep 2
+fi
+fi
+mode_check
+echo "Front End Switcheroo Currently Using $fe"
+sleep 3
+echo "Rebooting Now"
+sleep 3
+sudo reboot
+
+function mode_check() {
+if grep -q 'emulationstation \#auto' "$AUTOSTART"; then
+	fe="Emulationstation Mode"
+elif grep -q 'attract \#auto' "$AUTOSTART"; then
+	fe="Attract Mode"
+elif grep -q 'pegasus-fe \#auto' "$AUTOSTART"; then
+	fe="Pegasus Mode"
+elif grep -q 'startx \#auto' "$AUTOSTART"; then
+	fe="Desktop Mode"
+fi
+}
